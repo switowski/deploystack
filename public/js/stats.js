5ef1c3543b6e34da4aeaf906d7ccc01a0be0ddc3
Sebastian Witowski <witowski.sebastian@gmail.com>
2017-03-12 12:06:18 +0100

    Testing Atatus - Real User Monitoring

--- public/js/stats.js
+++ public/js/stats.js
@@ -13,3 +13,6 @@ ga('send', 'pageview');
 //- Heap Analytics - because it's easier to use than Google
 window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
 heap.load("2779816653");
+
+// Atatus - real users monitoring
+atatus.config('559699c377aa47c89c2f70ca25a747fc').install();
