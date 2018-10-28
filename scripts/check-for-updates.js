0c8f6a0cc0f06ea435fec8af69f4a7c9b36e5ec6
Sebastian Witowski <witowski.sebastian@gmail.com>
2018-10-28 18:28:53 +0100

    update domains again
    * 1&1 changed name today :/

--- scripts/check-for-updates.js
+++ scripts/check-for-updates.js
@@ -4,80 +4,52 @@ var puppeteer = require('puppeteer'),
 
 var URLs = require('./updates-urls.js');
 
-// For each URL, we can check multiple CSS selectors and compare if it's value
-// is correct
-async function checkWebsite(url, element, text) {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+// For each URL, we can check multiple CSS selectors and compare if it's value is correct
+async function checkWebsites(){
+  // Using for loops we can make the function wait for each async code to finish before the next iteration
+  // https://stackoverflow.com/questions/46293216/crawling-multiple-url-in-a-loop-using-puppeteer
+  for (let i = 0; i < URLs.length; i++) {
+    const website = URLs[i];
+    for (let j = 0; j < website.targets.length; j++) {
+      const element = website.targets[j].element;
+      const text = website.targets[j].value;
 
-  await page.goto(url);
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
 
-  const pageElements = await page.$x(element);
+      await page.goto(website.url);
 
-  console.log(chalk.blue('Testing "' + url + '" for text ' + text + ':'));
+      const pageElements = await page.$x(element);
 
-  if (pageElements.length == 0) {
-    console.log(chalk.red("Can't find element: " + element));
-    // Uncomment this line to save HTML in a file and stop the script.
-    const html = await page.content();
-    fs.writeFile('./html-body.html', html, function(err) {
-      console.log(chalk.red("Saved HTML to 'html-body.html' file!"));
-    });
+      console.log(chalk.blue('Testing "' + website.url + '" for text ' + text + ':'));
 
-    await browser.close();
-  } else {
-    let textOfElement = await page.evaluate(el => el.textContent, pageElements[0]);
+      if (pageElements.length == 0) {
+        console.log(chalk.red("Can't find element: " + element));
+        // Uncomment this line to save HTML in a file and stop the script.
+        const html = await page.content();
+        fs.writeFile('./html-body.html', html, function(err) {
+          console.log(chalk.red("Saved HTML to 'html-body.html' file!"));
+        });
 
-    if (textOfElement.trim() === text) {
-      console.log(chalk.green('OK'));
-    } else {
-      // Something changed, report it
-      console.log(chalk.red('Error in element:' + element));
-      console.log(chalk.red('Expected value:' + text));
-      console.log(chalk.red('Actual   value:' + textOfElement.trim()));
-      console.log(chalk.red('Actual value before trim:' + textOfElement));
+        await browser.close();
+      } else {
+        let textOfElement = await page.evaluate(el => el.textContent, pageElements[0]);
+
+        if (textOfElement.trim() === text) {
+          console.log(chalk.green('OK'));
+        } else {
+          // Something changed, report it
+          console.log(chalk.red('Error in element:' + element));
+          console.log(chalk.red('Expected value:' + text));
+          console.log(chalk.red('Actual   value:' + textOfElement.trim()));
+          console.log(chalk.red('Actual value before trim:' + textOfElement));
+        }
+
+        await browser.close();
+      }
     }
-
-    await browser.close();
   }
-};
-
+}
 
 console.log(chalk.yellow('Testing if any information on the website requires updates'));
-
-URLs.forEach(function(website) {
-  website.targets.forEach(function(target, index){
-    const url = website.url;
-    const element = target.element;
-    const value = target.value;
-
-    checkWebsite(url, element, value);
-  });
-});
-
-//     console.log(chalk.blue('Testing "' + website.url + '":'));
-
-//     $ = cheerio.load(body);
-//     website.targets.forEach(function(target, index){
-//       var element = $(target.element);
-//       console.log(chalk.blue('Checking: ' + index + '/' + website.targets.length));
-//       if ($(element).length === 0) {
-//         // Can't find the element on the page
-//         console.log(chalk.red("Can't find element: " + target.element));
-//         // Uncomment this line to save HTML in a file and stop the script.
-//         fs.writeFile('./html-body.html', body, function(err) {throw "Saved to a file!";});
-//       }
-//       else if ($(element).text().trim() != target.value) {
-//         // Something changed, report it
-//         console.log(chalk.red('Error in element: ' + element));
-//         console.log(chalk.red('Expected value: ' + target.value));
-//         console.log(chalk.red('Actual value: ' + $(element).text().trim()));
-//         console.log(chalk.red('Actual value before trim: ' + $(element).text()));
-//         // Uncomment this line to save HTML in a file and stop the script.
-//         // fs.writeFile('./html-body.html', body, function(err) {throw "Saved to a file!";});
-//       } else {
-//         console.log(chalk.green('OK'));
-//       }
-//     });
-//   });
-// });
+checkWebsites();
